3159477ab2df079c68f1eccc851f893efb52bf61
Antje Walter <115352305+AntjeWalter@users.noreply.github.com>
2022-11-24 14:13:23 +0100

    add list of shopping items (#1)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import ShoppingItem from "./components/ShoppingItem/ShoppingItem";
+import { useEffect, useState } from "react";
 
-function App() {}
+function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    async function startFetching() {
+      const response = await fetch("https://pokeapi.co/api/v2/item/");
+      const data = await response.json();
+      setItems(data.results);
+      console.log(data.results);
+    }
+
+    startFetching();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <ul>
+        {items.map((item) => (
+          <ShoppingItem name={item.name} key={item.name}>
+            {item.name}
+          </ShoppingItem>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 export default App;
